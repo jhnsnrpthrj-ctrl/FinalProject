a2243ae6c33bb926aeef6eae6ab487d20c305a2f
jhnsnrpthrj-ctrl <jhnsnrpthrj@gmail.com>
2026-01-16 09:47:21 +0530

    firstTimePush

---
+++ src/test/java/com/git/task1/GitTask1.java
@@ -0,0 +1,14 @@
+package com.git.task1;
+
+public class GitTask1 {
+	
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 5;
+		int c = a+b;
+		
+		System.out.println(c);
+	}
+
+}
---
+++ src/test/java/com/git/task1/GitTask2.java
@@ -0,0 +1,11 @@
+package com.git.task1;
+
+public class GitTask2 {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("task2");
+	}
+
+}
